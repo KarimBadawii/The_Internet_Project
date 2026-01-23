c373c72bdd220c21b082ba55c3db2f7f47e79922
KarimBadawii <badawy515@gmail.com>
2026-01-23 17:00:45 +0200

    feat: add takes Screenshot after test failure

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,11 +2,19 @@ package base;
 
 import Pages.HomePage;
 import Utils.WindowManager;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
     private WebDriver driver ;
@@ -29,6 +37,20 @@ public class BaseTests {
     @AfterClass
     public void tearDown(){
         driver.quit();
+    }
+
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            var takeScreenshot = (TakesScreenshot) driver;
+            File screenShot = takeScreenshot.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenShot, new File("resources/screenshots/"+result.getName()+".png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public WindowManager getWindowManager(){
